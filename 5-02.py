074ec3848ab32c251da6f3668a168abab9b372c3
Raymond <Raymond.octavious@ocsbstudent.ca>
2017-11-15 13:18:56 -0500

    Init

---
+++ 5-02.py
@@ -0,0 +1,16 @@
+#created by andre and ray, roman, and malcolm
+#created in november
+#created for isc3u
+#this program returns the largest value in the array
+
+
+def max_value(array = []):
+    
+    largest_value = max(array)
+    return largest_value
+    
+    
+the_array = [5,6,7]
+largest_value = max_value(the_array)
+print(the_array)
+print 'The largest value is:',largest_value
